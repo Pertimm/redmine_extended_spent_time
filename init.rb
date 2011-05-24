b6b9baccc19ffdece7eb1533d31c00709703200a
Jean-Marie Vallet <jm.vallet@gmail.com>
2011-05-24 17:39:45 +0200

    Add required Redmine version

--- init.rb
+++ init.rb
@@ -5,6 +5,7 @@ Redmine::Plugin.register :redmine_extended_spent_time do
   author 'Jean-Marie Vallet'
   description 'Redmine plugin used to extend spent time visualisation options located in "my page"'
   version '0.0.1'
+  requires_redmine :version => ['1.0.5']
   url 'https://github.com/jmvallet/redmine_extended_spent_time'
   author_url 'http://jmvallet.net/'
 end
