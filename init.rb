d5725323ce13d606d00197f44050fbd32fdbfd7e
Andrew Grangaard <granny-github@ofb.net>
2011-06-01 14:46:17 -0700

    Edited init.rb via GitHub

--- init.rb
+++ init.rb
@@ -6,6 +6,6 @@ Redmine::Plugin.register :redmine_extended_spent_time do
   description 'Redmine plugin used to extend spent time visualisation options located in "my page"'
   version '0.0.2'
   requires_redmine :version_or_higher => ['1.1.0']
-  url 'https://github.com/jmvallet/redmine_extended_spent_time'
+  url 'https://github.com/Pertimm/redmine_extended_spent_time'
   author_url 'http://jmvallet.net/'
 end
