20429f3385cca04a6c20a0ae9022560ec5209aea
Jean-Marie Vallet <jm.vallet@gmail.com>
2013-01-22 10:01:06 +0100

    Update to Redmine v2.2.1

---
+++ config/routes.rb
@@ -0,0 +1,1 @@
+match "extended_spent_time", to: "extended_spent_time#update", as: "extended_spent_time"
--- init.rb
+++ init.rb
@@ -4,8 +4,8 @@ Redmine::Plugin.register :redmine_extended_spent_time do
   name 'Redmine Extended Spent Time plugin'
   author 'Jean-Marie Vallet'
   description 'Redmine plugin used to extend spent time visualisation options located in "my page"'
-  version '0.0.2'
-  requires_redmine :version_or_higher => ['1.1.0']
+  version '0.0.3'
+  requires_redmine :version => ['2.2.1']
   url 'https://github.com/Pertimm/redmine_extended_spent_time'
   author_url 'http://jmvallet.net/'
 end
